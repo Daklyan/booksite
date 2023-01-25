b2d4f3745c01e4d0a3a070ff1355e571af307af8
daklyan <daklyan@protonmail.com>
2023-01-25 13:34:33 +0100

    test

--- website/scripts/register.js
+++ website/scripts/register.js
@@ -1,23 +1,23 @@
 const API_URL = `https://falconia.fr:4242`
 
-new VUE({
+let app = new Vue({
   el: '#app',
   data() {
     return {
       username: null,
-      password: null
+      password: null,
     }
   },
   methods: {
     async register(){
       resp = await fetch(`${API_URL}/add_user`, {
         method: 'POST',
-        headers: {'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-        body new URLSearchParams({
+        headers: {'Content-type': 'charset=UTF-8'},
+        body: new URLSearchParams({
             username: this.username,
             password: this.password
         })
-      })
+      }).then(reponse => reponse.json())
     }
   }
 })
